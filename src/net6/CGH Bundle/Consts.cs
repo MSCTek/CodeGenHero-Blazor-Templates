80686db2eaf57a65a077a1e6a93368ace81de3d3
Robert Schroeder <RobertS@msctek.com>
2022-06-21 11:13:03 -0500

    Fixed instances of Variable Descriptions lacking periods at the end, for consistency.

--- src/net6/CGH Bundle/Consts.cs
+++ src/net6/CGH Bundle/Consts.cs
@@ -67,7 +67,7 @@
         public const string PTG_WebApiDataServiceNamespace_DESC = "The namespace of the Web API Data Service classes this template will use. Should be promoted to Global.";
 
         public const string PTG_AppPageViewModelsNamespace_DEFAULT = "{BaseNamespace}.App.Pages";
-        public const string PTG_AppPageViewModelsNamespace_DESC = "The namespace of Razor Page viewmodels this template will use. Should be promoted to Global";
+        public const string PTG_AppPageViewModelsNamespace_DESC = "The namespace of Razor Page viewmodels this template will use. Should be promoted to Global.";
 
         #endregion Namespaces
 
@@ -105,10 +105,10 @@
         public const string PTG_ApiStatusControllerClassName_DESC = "The name of the API Status Controller class. Should be promoted to Global.";
 
         public const string PTG_AdminListPageViewModelClassName_DEFAULT = "{namespacePostfix}[tablepluralname]ListViewModel";
-        public const string PTG_AdminListPageViewModelClassName_DESC = "The name of the Admin List Page Viewmodel class. Should be promoted to Global";
+        public const string PTG_AdminListPageViewModelClassName_DESC = "The name of the Admin List Page Viewmodel class. Should be promoted to Global.";
 
         public const string PTG_AdminEditViewModelClassName_DEFAULT = "{namespacePostfix}[tablename]EditViewModel";
-        public const string PTG_AdminEditViewModelClassName_DESC = "The name of the Admin Edit Page Viewmodel class. Should be promoted to Global";
+        public const string PTG_AdminEditViewModelClassName_DESC = "The name of the Admin Edit Page Viewmodel class. Should be promoted to Global.";
 
         #endregion Class Names
 
